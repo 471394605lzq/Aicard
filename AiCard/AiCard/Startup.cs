12577ce11e36f2239a729ee67ef68729e46373ee
chao <sonic_458@126.com>
2018-10-03 11:53:18 +0800

    添加和更新NuGet

---
+++ AiCard/AiCard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AiCard.Startup))]
+namespace AiCard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
