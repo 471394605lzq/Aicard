649972166bf8b9899be2adb68e8d728c39f9cb1a
chao <sonic_458@126.com>
2018-10-03 14:59:08 +0800

    添加基础css

---
+++ AiCard/AiCard/Controllers/UserManageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AiCard.Models;
+namespace AiCard.Controllers
+{
+    public class UserManageController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UserManage
+        public ActionResult Index(string filter, Enums.UserType? userType, int page = 1)
+        {
+            var model = db.Users.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(filter))
+            {
+                model = model.Where(s => s.UserName == filter);
+            }
+            if (userType != null)
+            {
+                model = model.Where(s => s.UserType == userType);
+            }
+            var paged = model.ToPagedList(page);
+            return View(paged);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
