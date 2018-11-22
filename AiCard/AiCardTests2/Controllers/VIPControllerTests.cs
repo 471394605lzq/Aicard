1e89de709e32168df1ced6b68095eb312278f55f
lym <1049405388@qq.com>
2018-11-22 09:57:06 +0800

    暂存

--- AiCard/AiCardTests2/Controllers/VIPControllerTests.cs
+++ AiCard/AiCardTests2/Controllers/VIPControllerTests.cs
@@ -16,14 +16,16 @@ namespace AiCard.Controllers.Tests
         [TestMethod()]
         public void UpGradeVIPTest()
         {
-            VIPController vip = new VIPController();
+            //VIPController vip = new VIPController();
             //ActionResult result = vip.UpGradeVIP("aa", "01f1704d-fd1f-409d-a3d1-eb23f2fd6415");
 
-            OrderBLL bll = new OrderBLL();
+            //OrderBLL bll = new OrderBLL();
             //string s = bll.CreateOrderCode("291afe55-6091-49ba-842b-de4b4aeffce7");
             //int leng = s.Length;
-            ActionResult obj = vip.Refund("2018112011422870401f1704d38996", "01f1704d-fd1f-409d-a3d1-eb23f2fd64");
+            //ActionResult obj = vip.Refund("2018112011422870401f1704d38996", "01f1704d-fd1f-409d-a3d1-eb23f2fd64");
 
+            CardPersonalController card = new CardPersonalController();
+            ActionResult result = card.Create("011c7a5f-c201-4906-ac4c-6a0ee5372c7a", "15521110070");
         }
     }
 }
