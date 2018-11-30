daf23cac2449a59e942696efa7887ede3f8fbddf
lzq <dtkj001@DESKTOP-FLGF1SV>
2018-11-30 19:42:13 +0800

    修改智能追踪接口

--- AiCard/AiCard/Models/CardViewModels.cs
+++ AiCard/AiCard/Models/CardViewModels.cs
@@ -4,6 +4,7 @@ using AiCard.Common.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -113,7 +114,7 @@ namespace AiCard.Models
 
         [Display(Name = "排序")]
         public int Sort { get; set; }
-        public string ActiveImage { get; set; }
+
     }
 
     public class CardListViewModel
@@ -218,6 +219,87 @@ namespace AiCard.Models
     }
 
 
+    public class CardShowModel
+    {
+        public int ID { get; set; }
 
+        [Display(Name = "企业")]
+        public int? EnterpriseID { get; set; }
+
+        public string EnterpriseName { get; set; }
+
+        [Display(Name = "用户")]
+        public string UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        [Display(Name = "企业微信ID")]
+        public string WeChatEID { get; set; }
+
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+
+        [Display(Name = "头像")]
+        public string Avatar { get; set; }
+
+        [Display(Name = "座机")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "微信号")]
+        public string WeChatCode { get; set; }
+
+        [Display(Name = "手机号")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "启用")]
+        public bool Enable { get; set; }
+
+        [Display(Name = "职称")]
+        public string Position { get; set; }
+
+        [Display(Name = "性别")]
+        public Gender Gender { get; set; }
+
+        [Display(Name = "签名")]
+        public string Remark { get; set; }
+
+        [Display(Name = "信息")]
+        public string Info { get; set; }
+
+        [Display(Name = "语音")]
+        public string Voice { get; set; }
+
+        [Display(Name = "视频")]
+        public string Video { get; set; }
+
+        [Display(Name = "图片")]
+        public string Images { get; set; }
+
+        [Display(Name = "小程序分享二维码")]
+        public string WeChatMiniQrCode { get; set; }
+
+        [Display(Name = "海报")]
+        public string Poster { get; set; }
+
+        [Display(Name = "行业")]
+        public string Industry { get; set; }
+
+        [Display(Name = "点赞")]
+        public int Like { get; set; }
+
+        [Display(Name = "阅览数")]
+        public int View { get; set; }
+
+        [Display(Name = "生日")]
+        public DateTime? Birthday { get; set; }
+
+        [Display(Name = "排序")]
+        public int Sort { get; set; }
+        [Display(Name = "激活二维码")]
+        public string ActiveImage { get; set; }
+    }
 
 }
