fd82ff6808e13b186bdc97ab1d3e0e9b2c106bae
lym <1049405388@qq.com>
2018-12-01 10:06:20 +0800

    添加提现接口

---
+++ AiCard/AiCard/Models/VipForward/MReqBankForward.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AiCard.Models.VipForward
+{
+    /// <summary>
+    /// 申请提现的参数
+    /// </summary>
+    public sealed class MReqBankForward
+    {
+        /// <summary>
+        /// 提现入账账户ID
+        /// </summary>
+        public int bankAccountID { get; set; }
+        /// <summary>
+        /// 提现金额
+        /// </summary>
+        public decimal forwardAmount { get; set; }
+    }
+}
