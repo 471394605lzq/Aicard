b500d764e8123d3cdf786b8a0de92d6cdbf98317
lym <1049405388@qq.com>
2018-12-01 10:07:18 +0800

    Revert "添加提现接口"
    This reverts commit fd82ff6808e13b186bdc97ab1d3e0e9b2c106bae.

--- AiCard/AiCard/Bll/VipForwardBLL.cs
+++ AiCard/AiCard/Bll/VipForwardBLL.cs
@@ -1,10 +1,7 @@
-﻿using AiCard.Common;
-using AiCard.Models.VipForward;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using static AiCard.Common.Comm;
 
 namespace AiCard.Bll
 {
@@ -15,40 +12,8 @@ namespace AiCard.Bll
     /// </summary>
     public sealed class VipForwardBLL
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        /// <param name="vUserID"></param>
-        /// <returns></returns>
-        public RequestResult VipForwardApply(MReqBankForward model,string vUserID)
-        {
-            #region
-            RequestResult result = new RequestResult()
-            {
-                retCode = ReqResultCode.failed,
-                retMsg = "提现申请失败"
-            };
+        //public object VipForwardApply() {
 
-            int rows = 0;
-            try
-            {
-         
-                
-                
-                       
-
-            }
-            catch (Exception ex)
-            {
-                result.retCode = ReqResultCode.excetion;
-                result.retMsg = $"申请提现时发生异常：{ex.Message}";
-                //调试日志
-                Comm.WriteLog("exception", result.retMsg, Common.Enums.DebugLogLevel.Error, "Bll.VipForwardBLL.VipForwardApply");
-            }
-
-            return result;
-            #endregion
-        }
+        //}
     }
 }
