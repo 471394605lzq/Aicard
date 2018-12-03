a1ab084a2269c513b5a791244f685f88e226d38c
chao <sonic_458@126.com>
2018-12-03 09:19:30 +0800

    给IdentityModels加备注

--- AiCard/AiCard.DAL/Models/IdentityModels.cs
+++ AiCard/AiCard.DAL/Models/IdentityModels.cs
@@ -52,7 +52,9 @@ namespace AiCard.DAL.Models
         public string OpenIDs { get; set; }
     }
 
-
+    /// <summary>
+    /// 权限
+    /// </summary>
     public class ApplicationRole : IdentityRole
     {
         public ApplicationRole() : base() { }
@@ -75,64 +77,144 @@ namespace AiCard.DAL.Models
 
         }
 
+        /// <summary>
+        /// 角色
+        /// </summary>
         public DbSet<RoleGroup> RoleGroups { get; set; }
 
+        /// <summary>
+        /// 企业
+        /// </summary>
         public DbSet<Enterprise> Enterprises { get; set; }
 
+        /// <summary>
+        /// 企业名片
+        /// </summary>
         public DbSet<Card> Cards { get; set; }
 
+        /// <summary>
+        /// 动态
+        /// </summary>
         public DbSet<Article> Articles { get; set; }
 
+        /// <summary>
+        /// 动态评论
+        /// </summary>
         public DbSet<ArticleComment> ArticleComments { get; set; }
 
+        /// <summary>
+        /// 用户行为记录
+        /// </summary>
         public DbSet<UserLog> UserLogs { get; set; }
 
+        /// <summary>
+        /// 企业版商品
+        /// </summary>
         public DbSet<Product> Products { get; set; }
 
+        /// <summary>
+        /// 企业商品分类
+        /// </summary>
         public DbSet<ProductKind> ProductKinds { get; set; }
 
+        /// <summary>
+        /// 企业版自定义主页模块
+        /// </summary>
         public DbSet<HomePageModular> HomePageModulars { get; set; }
 
+        /// <summary>
+        /// 名片标签
+        /// </summary>
         public DbSet<CardTab> CardTabs { get; set; }
 
-
+        /// <summary>
+        /// 商品置顶
+        /// </summary>
         public DbSet<UserProductTop> UserProductTops { get; set; }
 
+        /// <summary>
+        /// 用户名片置顶
+        /// </summary>
         public DbSet<UserCardTop> UserCardTops { get; set; }
 
+        /// <summary>
+        /// 企业客户
+        /// </summary>
         public DbSet<EnterpriseCustomer> EnterpriseCustomers { get; set; }
 
+        /// <summary>
+        /// 企业客户备注
+        /// </summary>
         public DbSet<EnterpriseUserCustomer> EnterpriseUserCustomer { get; set; }
 
+        /// <summary>
+        /// 企业预设标签
+        /// </summary>
         public DbSet<CustomerTab> CustomerTabs { get; set; }
 
+        /// <summary>
+        /// 企业的预设标签分组
+        /// </summary>
         public DbSet<CustomerTabGroup> CustomerTabGroups { get; set; }
 
+        /// <summary>
+        /// 企业客户的标签
+        /// </summary>
         public DbSet<EnterpriseCustomerTab> EnterpriseCustomerTabs { get; set; }
 
+        /// <summary>
+        /// 语术
+        /// </summary>
         public DbSet<UserSpeech> UserSpeechs { get; set; }
 
+        /// <summary>
+        /// 语术类别
+        /// </summary>
         public DbSet<UserSpeechType> UserSpeechTypes { get; set; }
 
-
+        /// <summary>
+        /// 交易订单
+        /// </summary>
         public DbSet<Order> Orders { get; set; }
 
+        /// <summary>
+        /// VIP（个人版用户扩展表）
+        /// </summary>
         public DbSet<Vip> Vips { get; set; }
 
-
+        /// <summary>
+        /// VIP的关系记录
+        /// </summary>
         public DbSet<VipRelationship> VipRelationships { get; set; }
 
+        /// <summary>
+        /// VIP的金额记录
+        /// </summary>
         public DbSet<VipAmountLog> VipAmountLogs { get; set; }
 
+        /// <summary>
+        /// VIP提现
+        /// </summary>
         public DbSet<VipForwardOrder> VipForwardOrders { get; set; }
 
+        /// <summary>
+        /// 商品置顶
+        /// </summary>
         public DbSet<CardUserProductTop> CardUserProductTops { get; set; }
 
-
+        /// <summary>
+        /// 个人名片
+        /// </summary>
         public DbSet<CardPersonal> CardPersonals { get; set; }
 
+        /// <summary>
+        /// 个人版VIP提现账号
+        /// </summary>
         public DbSet<VipForwardAccount> VipForwardAccounts { get; set; }
 
+        /// <summary>
+        /// 验证码
+        /// </summary>
         public DbSet<VerificationCode> VerificationCodes { get; set; }
 
 
