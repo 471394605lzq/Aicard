ad9488b9c7ad77f0702000323138a098ff1dceea
Administrator <Administrator@SC-201809291647>
2018-12-06 00:46:41 +0800

    修复收款的消息推送

--- AiCard/AiCard.Commom/WeChat/IWeChatMessageTemp.cs
+++ AiCard/AiCard.Commom/WeChat/IWeChatMessageTemp.cs
@@ -23,7 +23,7 @@ namespace AiCard.Common.WeChat.WeChatMessageTemp
         {
             data = new
             {
-                keyword1 = new { value = (int)(amount * 100) },
+                keyword1 = new { value = $"{amount}元" },
                 keyword2 = new { value = remark },
                 keyword3 = new { value = dateTime.ToString("yyyy-MM-dd HH:mm:ss") }
             };
