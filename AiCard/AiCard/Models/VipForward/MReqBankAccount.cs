c91b0fa74df79164894846c46f9340af54ddea1a
lym <1049405388@qq.com>
2018-12-06 20:25:27 +0800

    更改提现银行卡认证绑定接口

---
+++ AiCard/AiCard/Models/VipForward/MReqBankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AiCard.Models.VipForward
+{
+    /// <summary>
+    /// 绑定银行卡参数
+    /// </summary>
+    public sealed class MReqBankAccount
+    {
+        /// <summary>
+        /// 银行名称
+        /// </summary>
+        public string bankName { get; set; }
+        
+        /// <summary>
+        /// 银行代码
+        /// </summary>
+        public string bankCode { get; set; }
+        /// <summary>
+        /// 银行卡号
+        /// </summary>
+        public string cardNo { get; set; }
+        /// <summary>
+        /// 身份证号码
+        /// </summary>
+        public string idNo { get; set; }
+        /// <summary>
+        /// 开户名
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string phoneNo { get; set; }
+
+        /// <summary>
+        /// 签名
+        /// </summary>
+        public string sign { get; set; }
+
+    }
+}
